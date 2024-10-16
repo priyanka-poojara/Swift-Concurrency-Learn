9106d1841ae32d19eed5bca74cb28059ccdc9114
Priyanka Poojara <poojarapriyanka2305@gmail.com>
2024-10-16 13:04:54 +0530

    Initial commit

---
+++ ConcurrencySwift/ConcurrencySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencySwiftApp.swift
+//  ConcurrencySwift
+//
+//  Created by Priyanka on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcurrencySwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConcurrencySwift
+//
+//  Created by Priyanka on 16/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
